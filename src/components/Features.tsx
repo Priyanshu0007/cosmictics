609a04b8570a3a553b06b42c30138d386a26db56
Priyanshu0007 <63500076+Priyanshu0007@users.noreply.github.com>
2023-10-09 02:11:11 +0530

    feature section added

---
+++ src/components/Features.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import FeaturesCard from './FeaturesCard'
+
+const data=[
+    {
+        img:"/feature/sls.png",
+        title:"SLS Free",
+        desc:"SLS Free"
+    },
+    {
+        img:"/feature/fda.png",
+        title:"FDA Approved",
+        desc:"Natural and organic products",
+    },
+    {
+        img:"/feature/dermo.png",
+        title:"Lab Tested",
+        desc:"Safe Products",
+    },
+    {
+        img:"/feature/plastic.png",
+        title:"Plastic Free",
+        desc:"Environmental Friendly",
+    }
+]
+const Features = () => {
+  return (
+    <div className='container pt-16'>
+        <div className='grid md:grid-cols-2 gap-y-8 lg:gap-y04 lg:grid-cols-4 gap-4'>
+            {data.map(item=><FeaturesCard key={item.title} img={item.img} title={item.title} desc={item.desc}/>)}
+        </div>
+    </div>
+  )
+}
+
+export default Features
---
+++ src/components/FeaturesCard.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import React from 'react'
+
+interface propsType{
+    img:string;
+    title:string;
+    desc:string;
+}
+const FeaturesCard = ({img,title,desc}:propsType) => {
+  return (
+    <div className='flex gap-8'>
+        <Image src={img} className='h-[50px] w-auto' width={60} height={50} alt={title}/>
+        <div className='space-y-1'>
+            <h2 className='font-medium text-xl uppercase'>{title}</h2>
+            <p className='text-gray-600 text-[14px]'>{desc}</p>
+        </div>
+    </div>
+  )
+}
+
+export default FeaturesCard

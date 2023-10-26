87035f1743455ba38eaec9e4c7142794bd342fe7
Priyanshu0007 <63500076+Priyanshu0007@users.noreply.github.com>
2023-10-26 21:43:16 +0530

    persistance added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./fetaures/cartSlice";
-import favReducer from "./fetaures/favSlice"
+import favReducer from "./fetaures/favSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+const persistConfig={
+    key:"root",
+    version:1,
+    storage
+}
+const reducer= combineReducers({
+    cartReducer,
+    favReducer,
+});
+const persistedReducer=persistReducer(persistConfig,reducer);
 export const store=configureStore({
-    reducer:{cartReducer,favReducer},
+    reducer:persistedReducer,
     devTools:process.env.NODE_ENV!=="production",
 });
 export type RootState=ReturnType<typeof store.getState>;
